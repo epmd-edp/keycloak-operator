8fc1a4667801162cd8c0bf3ba25a6366e2d7a8a7
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2020-03-23 15:46:52 +0200

    [EPMDEDP-4826] Extend KeycloakClient API with DefaultClientScopeRequired property
    Change-Id: I51e7daf1b3f7865ffd943d9a20b908a132fb7e1a

--- pkg/apis/v1/v1alpha1/keycloakclient_types.go
+++ pkg/apis/v1/v1alpha1/keycloakclient_types.go
@@ -10,15 +10,16 @@ import (
 // KeycloakClientSpec defines the desired state of KeycloakClient
 // +k8s:openapi-gen=true
 type KeycloakClientSpec struct {
-	TargetRealm             string       `json:"targetRealm"`
-	Secret                  string       `json:"secret"`
-	RealmRoles              *[]RealmRole `json:"realmRoles,omitempty"`
-	Public                  bool         `json:"public"`
-	ClientId                string       `json:"clientId"`
-	WebUrl                  string       `json:"webUrl"`
-	DirectAccess            bool         `json:"directAccess"`
-	AdvancedProtocolMappers bool         `json:"advancedProtocolMappers"`
-	ClientRoles             []string     `json:"clientRoles, omitempty"`
+	TargetRealm                string       `json:"targetRealm"`
+	Secret                     string       `json:"secret"`
+	RealmRoles                 *[]RealmRole `json:"realmRoles,omitempty"`
+	Public                     bool         `json:"public"`
+	ClientId                   string       `json:"clientId"`
+	WebUrl                     string       `json:"webUrl"`
+	DirectAccess               bool         `json:"directAccess"`
+	AdvancedProtocolMappers    bool         `json:"advancedProtocolMappers"`
+	ClientRoles                []string     `json:"clientRoles, omitempty"`
+	DefaultClientScopeRequired bool         `json:"defaultClientScopeRequired"`
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
